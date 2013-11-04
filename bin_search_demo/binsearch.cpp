662b1112a53b539858d6c332b9fd0dbca5b6d873
lhfcws <lhfcws@163.com>
2013-11-04 23:44:01 +0800

    hw3 for wuxiangjun class

---
+++ bin_search_demo/binsearch.cpp
@@ -0,0 +1,41 @@
+#include <cstdio>
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int n;
+int a[30];
+int key;
+
+int bin_search(int head, int tail) {
+    /* Cannot find element. */
+    if (head > tail)
+        return -1;
+
+    int mid = (head + tail) / 2;
+
+    /* Quick find */
+    // if (a[mid] == key) return mid;
+    
+
+    /* Another terminate condition */
+    if (head == tail) return head;
+
+
+    if (a[mid] >= key)
+        return bin_search(head, mid);
+    else if (a[mid] < key)
+        return bin_search(mid + 1, tail);
+
+}
+
+int main() {
+    /* Read data */
+    cin >> n;
+    for (int i = 0; i < n; i++) 
+        cin >> a[i];
+    
+    key = 3;
+    cout << bin_search(0, n-1) << endl;
+}
